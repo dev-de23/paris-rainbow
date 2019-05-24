4a8b869a5e1ed954c39168704fc7d2db621f51b5
dev-de23 <50382811+dev-de23@users.noreply.github.com>
2019-05-24 08:08:31 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,23 @@
+﻿const Discord = require("discord.js");
+const client = new Discord.Client();
+
+
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`batatabot By:Batata`);
+        console.log(`----------------`);
+      console.log(`Batata '     Script By : batata ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setStatus("dnd")
+});
+
+
+client.on("guildMemberAdd", member => {
+  client.channels.find('id', 'أي دي الشات').send(`**Wel``come`` To C``reator``**`)
+});
+
+
+
+client.login(process.env.BOT_TOKEN);
